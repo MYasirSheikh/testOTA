e93937d90ea115cf4ae939971b4f9708dab63174
M Yasir <m.yasir234@gmail.com>
2018-04-11 11:53:05 +0500

    env port added

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello')
-})
+    res.send('Hello');
+});
 
 app.get('/fw2_ota', (req, res) => {
     console.log(req);
-    res.sendFile('http://test-ota.herokuapp.com/firmwareOTAServer/public/main.ino.bin')
-})
+    res.sendFile('http://test-ota.herokuapp.com/firmwareOTAServer/public/main.ino.bin');
+});
 
-app.listen(80);
-console.log('listening on port 1234')
+app.listen(process.env.PORT || 8000);
+console.log('listening on port 1234');
